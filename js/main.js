e6224e549a073d475fa829236dde6a2a6ec4cdcc
Jairiana de Jesus <jairianadejesus2005@gmail.com>
2023-10-28 12:33:13 -0300

    Accion al formulario del contenido a travez del javascript

--- js/main.js
+++ js/main.js
@@ -5,6 +5,32 @@ const opcionesMenuClosed = document.querySelector(".closed-opciones")
 opciones.addEventListener('click', abrirOpciones)
 
 function abrirOpciones () {
-    console.log('abrio el menu')
+    console.log('Click menu')
     opcionesMenu.classList.toggle('inactive')
 }
+
+// textarea
+document.getElementById('miFormularioTextarea').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe normalmente
+
+    // Obtén los datos del formulario
+    const formData = new FormData(this);
+
+    // Enviar datos a FormSubmit usando fetch
+    fetch('https://formspree.io/f/xwkdyzqj', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => {
+        // Verifica el código de estado de la respuesta
+        if (response.status === 200) {
+            alert('¡Formulario enviado con éxito!');
+        } else {
+            alert('Error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
+        }
+    })
+    .catch(error => {
+        console.error(error);
+        alert('Error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
+    });
+});
